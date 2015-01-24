3c23d80e1fc6ab39d67e308a7f82a50761a18e63
zac1st1k <zacfirst@gmail.com>
2015-01-25 01:38:46 +1100

    + Setup MenuViewController

---
+++ World Traveller/XZZMenuViewController.h
@@ -0,0 +1,15 @@
+//
+//  XZZMenuViewController.h
+//  World Traveller
+//
+//  Created by Zac on 25/01/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XZZMenuViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+
+@end
